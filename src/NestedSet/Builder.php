fe5fc7d0d889bb54b46a2bce5c979742874a99e6
SERGEY <xcreativ@ya.ru>
2024-04-12 14:06:45 +0700

    change save to insert in rebuild, add typehint, support for laravel 11.x

--- src/NestedSet/Builder.php
+++ src/NestedSet/Builder.php
@@ -4,12 +4,11 @@ namespace Baum\NestedSet;
 
 class Builder
 {
-
     /**
-     * Node instance for reference
-    *
-    * @var \Baum\Node
-    */
+     * Node instance for reference.
+     *
+     * @var \Baum\Node
+     */
     protected $node;
 
     /**
@@ -22,27 +21,68 @@ class Builder
     /**
      * Create a new \Baum\SetBuilder class instance.
      *
-     * @param   \Baum\Node      $node
-     * @return  void
+     * @param \Baum\Node $node
      */
+    protected array $rebuilds = [];
+
+    protected array $rebuildsAttributes = [];
+
     public function __construct($node)
     {
         $this->node = $node;
     }
 
+    protected function setRebuildsAttributes(): array
+    {
+        $columns = $this->node->getConnection()->getDoctrineSchemaManager()->listTableColumns($this->node->getTable());
+
+        $notNullableColumns = \array_keys(
+            \array_filter($columns, fn ($column) => $column->getNotnull() === true && $column->getDefault() === null)
+        );
+
+        return $this->rebuildsAttributes = array_unique([...$notNullableColumns,
+            $this->node->getParentColumnName(),
+            $this->node->getLeftColumnName(),
+            $this->node->getRightColumnName(),
+            $this->node->getKeyName(),
+            $this->node->getDepthColumnName(),
+        ]);
+    }
+
     /**
      * Perform the re-calculation of the left and right indexes of the whole
      * nested set tree structure.
-     *
-     * @param  bool $force
-     * @return void
      */
-    public function rebuild($force = false)
+
+    // public function rebuild($force = false)
+    // {
+    //     $alreadyValid = forward_static_call([get_class($this->node), 'isValidNestedSet']);
+    //
+    //     // Do not rebuild a valid Nested Set tree structure
+    //     if ( ! $force && $alreadyValid) {
+    //         return true;
+    //     }
+    //
+    //     // Rebuild lefts and rights for each root node and its children (recursively).
+    //     // We go by setting left (and keep track of the current left bound), then
+    //     // search for each children and recursively set the left index (while
+    //     // incrementing that index). When going back up the recursive chain we start
+    //     // setting the right indexes and saving the nodes...
+    //     $roots = $this->roots();
+    //
+    //     $this->node->getConnection()->transaction(function () use ($roots) {
+    //         foreach ($roots as $r) {
+    //             $this->rebuildBounds($r, 0);
+    //         }
+    //     });
+    // }
+
+    public function rebuild(bool $force = false): bool
     {
         $alreadyValid = forward_static_call([get_class($this->node), 'isValidNestedSet']);
 
         // Do not rebuild a valid Nested Set tree structure
-        if (!$force && $alreadyValid) {
+        if ( ! $force && $alreadyValid) {
             return true;
         }
 
@@ -51,12 +91,21 @@ class Builder
         // search for each children and recursively set the left index (while
         // incrementing that index). When going back up the recursive chain we start
         // setting the right indexes and saving the nodes...
+        $this->setRebuildsAttributes();
+
         $roots = $this->roots();
 
         $this->node->getConnection()->transaction(function () use ($roots) {
             foreach ($roots as $r) {
                 $this->rebuildBounds($r, 0);
             }
+
+            $updateAttributes = \array_filter($this->rebuildsAttributes, fn ($attr) => $attr != $this->node->getKeyName());
+
+            $this->node->upsert($this->rebuilds,
+                $this->node->getKeyName(),
+                $updateAttributes
+            );
         });
     }
 
@@ -72,14 +121,32 @@ class Builder
             ->orderBy($this->node->getQualifiedLeftColumnName())
             ->orderBy($this->node->getQualifiedRightColumnName())
             ->orderBy($this->node->getQualifiedKeyName())
-            ->get();
+            ->get()
+        ;
     }
 
     /**
      * Recompute left and right index bounds for the specified node and its
      * children (recursive call). Fill the depth column too.
      */
-    public function rebuildBounds($node, $depth = 0)
+
+    // public function rebuildBounds($node, $depth = 0)
+    // {
+    //     $k = $this->scopedKey($node);
+    //
+    //     $node->setAttribute($node->getLeftColumnName(), $this->getNextBound($k));
+    //     $node->setAttribute($node->getDepthColumnName(), $depth);
+    //
+    //     foreach ($this->children($node) as $child) {
+    //         $this->rebuildBounds($child, $depth + 1);
+    //     }
+    //
+    //     $node->setAttribute($node->getRightColumnName(), $this->getNextBound($k));
+    //
+    //     $node->save();
+    // }
+
+    public function rebuildBounds($node, int $depth = 0): void
     {
         $k = $this->scopedKey($node);
 
@@ -92,14 +159,15 @@ class Builder
 
         $node->setAttribute($node->getRightColumnName(), $this->getNextBound($k));
 
-        $node->save();
+        $this->rebuilds[] = $node->only($this->rebuildsAttributes);
     }
 
     /**
      * Return all children for the specified node.
      *
-     * @param   Baum\Node $node
-     * @return  Illuminate\Database\Eloquent\Collection
+     * @param Baum\Node $node
+     *
+     * @return Illuminate\Database\Eloquent\Collection
      */
     public function children($node)
     {
@@ -124,8 +192,9 @@ class Builder
     /**
      * Return an array of the scoped attributes of the supplied node.
      *
-     * @param   Baum\Node $node
-     * @return  array
+     * @param Baum\Node $node
+     *
+     * @return array
      */
     protected function scopedAttributes($node)
     {
@@ -146,8 +215,9 @@ class Builder
      * Return a string-key for the current scoped attributes. Used for index
      * computing when a scope is defined (acsts as an scope identifier).
      *
-     * @param   Baum\Node $node
-     * @return  string
+     * @param Baum\Node $node
+     *
+     * @return string
      */
     protected function scopedKey($node)
     {
@@ -164,14 +234,15 @@ class Builder
     }
 
     /**
-     * Return next index bound value for the given key (current scope identifier)
+     * Return next index bound value for the given key (current scope identifier).
      *
-     * @param   string  $key
-     * @return  integer
+     * @param string $key
+     *
+     * @return int
      */
     protected function getNextBound($key)
     {
-        if (false === array_key_exists($key, $this->bounds)) {
+        if (array_key_exists($key, $this->bounds) === false) {
             $this->bounds[$key] = 0;
         }
 
